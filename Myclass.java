6daceeb91df5929e9bdf7993897dba3caf8f2249
bishal <46195146+bishal-dhakal@users.noreply.github.com>
2023-02-25 06:18:38 +0545

    Add files via upload

---
+++ Myclass.java
@@ -0,0 +1,17 @@
+public class Myclass{
+    public static void main(String[] args){
+     Student dipson = new Student();
+     dipson.age=24;
+     dipson.id = 1;
+     dipson.name ="Dipson";
+
+     System.out.println(dipson.name +" is " + dipson.age + " years old" );
+
+     Student jyoti = new Student();
+     jyoti.age=24;
+     jyoti.id = 1;
+     jyoti.name ="Jyoti";
+
+     System.out.println(jyoti.name +" is " + jyoti.age + " years old" );
+    }
+}
